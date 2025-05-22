a766464fe33c46c9d54a5c35771d7cf872693a42
Chike <ezeh.chike@gmail.com>
2025-05-22 15:17:39 -0700

    create a basic function and use it in the main function.

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,18 +1,11 @@
 package main
 import "fmt"
 func main() {
-	// This is a simple Go program that does nothing.
-	// It serves as a placeholder for future development.
-	var intNum int = 90
-	fmt.Println(intNum)
-	var floatNum float64 = 12345678.9
-	fmt.Println(floatNum)
-	
-	var floatNum32 float32 = 10.1
-	var intNum32 int32 = 2
-	var result float32 = floatNum32 + float32(intNum32)
-	fmt.Println(result)
-	var str string = "Hello, World!"
-	fmt.Println(str)
+	printMe("Hello world")
+}
+
+func printMe(printValue string) {
+	// This function prints the value passed to it.
+	fmt.Println(printValue)
 
 }
