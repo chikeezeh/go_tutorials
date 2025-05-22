9e0e8ee7b9985b57cce54c42c15c7e3d09522ad1
Chike <ezeh.chike@gmail.com>
2025-05-21 17:32:16 -0700

    Create hello world in go

---
+++ cmd/tutorial_1/main.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main() {
+	// This is a simple Go program that does nothing.
+	// It serves as a placeholder for future development.
+	fmt.Println("Hello, World!")
+}
