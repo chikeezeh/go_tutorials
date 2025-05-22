87a17a6bd6e04d908a9eddbf544a8879cb7b50a1
Chike <ezeh.chike@gmail.com>
2025-05-21 17:45:53 -0700

    different variable types in go

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -3,5 +3,16 @@ import "fmt"
 func main() {
 	// This is a simple Go program that does nothing.
 	// It serves as a placeholder for future development.
-	fmt.Println("Hello, World!")
+	var intNum int = 90
+	fmt.Println(intNum)
+	var floatNum float64 = 12345678.9
+	fmt.Println(floatNum)
+	
+	var floatNum32 float32 = 10.1
+	var intNum32 int32 = 2
+	var result float32 = floatNum32 + float32(intNum32)
+	fmt.Println(result)
+	var str string = "Hello, World!"
+	fmt.Println(str)
+
 }
